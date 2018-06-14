8e1d85b5cf464271733d13c571b41081b7a518de
misspia <pia_leung@hotmail.com>
2018-06-13 22:18:56 -0400

    add hemi + directional light, create curve + spline

--- src/sketches/S007/sketch.js
+++ src/sketches/S007/sketch.js
@@ -8,16 +8,26 @@ import { Images } from '../../themes/themes.js';
 class Sketch extends SketchManagerThree {
   constructor(canvas) {
     super(canvas);
+    this.curve = {};
+    this.spline = {};
     this.tunnel = {};
     this.geometry = {};
     this.material = {};
-    this.spotLight = {};
-    this.ambientLight = {};
     this.tunnelSpeed = 1;
   }
   init() {
     this.setClearColor(0xf1ebeb);
+    this.createLight();
+    this.createCurve();
+    this.createSpline();
     this.createTunnel();
+  }
+  createLight() {
+    const hemi = new THREE.HemisphereLight(0xffffbb, 0x887979, 0.9);
+    this.scene.add(hemi);
+
+    const directional = new THREE.DirectionalLight(0xffffff, 0.8);
+    this.scene.add(directional);
   }
   createTunnel() {
     this.geometry = this.createTunnelGeometry();
@@ -30,15 +40,25 @@ class Sketch extends SketchManagerThree {
     const tube = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(tube);
   }
-  createTunnelGeometry() {
+  createCurve() {
     let points = [];
-
     for (let i = 0; i < 5; i += 1) {
       points.push(new THREE.Vector3(0, 0, 2.5 * (i / 4)));
     }
-    const curve = new THREE.CatmullRomCurve3(points)
+    points[4].y = -0.06;
+    this.curve = new THREE.CatmullRomCurve3(points);
+  }
+  createSplineMesh() {
+    const material = new THREE.LineBasicMaterial();
+    const geometry = new THREE.Geometry();
+    geometry.vertices = curve.getPoints(70);
+    this.spline = new THREE.Line(geometry, material);
+  }
+  createTunnelGeometry() {
 
-    const spline = new THREE.SplineCurve3(points);
+
+
+    // const spline = new THREE.SplineCurve3(points);
     return new THREE.TubeGeometry(curve, 70, 0.02, 50, false);
   }
   createTunnelMaterial() {
@@ -49,7 +69,10 @@ class Sketch extends SketchManagerThree {
     });
   }
   updateMaterialOffset() {
-    this.material.map.offset.x += this.tunnelSpeed;
+    // this.material.map.offset.x += this.tunnelSpeed;
+    this.material.map.offset.y += 100;
+    this.material.map.offset.x += 100;
+    // console.log(this.material.map.offset);
   }
   draw() {
     this.updateMaterialOffset();
