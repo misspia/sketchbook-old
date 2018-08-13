d1e6c93940348724fc1fa0359387ef38a4a773b1
misspia <pia_leung@hotmail.com>
2018-08-12 23:46:31 -0400

    init s008 directory

---
+++ src/sketches/S008/sketch.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+import vert from './vertex.glsl';
+import frag from './fragment.glsl';
+import SketchManagerThree from '../sketchManagerThree.js';
+
+class Sketch extends SketchManagerThree {
+  constructor() {
+    super();
+  }
+  init() {
+
+  }
+  draw() {
+
+  }
+}
+
+export default Sketch;
