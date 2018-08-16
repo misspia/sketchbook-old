b5f89f01b19a9d6df57c259d9ed8c364cd4b3223
misspia <pia_leung@hotmail.com>
2018-08-15 22:47:27 -0400

    init three mesh

--- src/sketches/S008/sketch.js
+++ src/sketches/S008/sketch.js
@@ -4,14 +4,33 @@ import frag from './fragment.glsl';
 import SketchManagerThree from '../sketchManagerThree.js';
 
 class Sketch extends SketchManagerThree {
-  constructor() {
-    super();
+  constructor(canvas) {
+    super(canvas);
+    this.geometry = {};
+    this.material = {};
+
   }
   init() {
-
+    this.setClearColor(0xf1ebeb);
+    this.createCenterPiece();
+  }
+  createCenterPiece() {
+    this.geometry = new THREE.IcosahedronGeometry(1, 2);
+    this.material = new THREE.RawShaderMaterial({
+      vertexShader: vert,
+      fragmentShader: frag,
+      uniforms: {
+        u_time: { type: 'f', value: 0 },
+      }
+    });
+    const sphere = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(sphere);
   }
   draw() {
+    this.material.uniforms.u_time.value = this.getUTime();
 
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(() => this.draw());
   }
 }
 
