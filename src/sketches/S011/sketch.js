9e754d3b3a1d391e1fdb423b0b723495ecebf466
misspia <pia_leung@hotmail.com>
2019-01-09 22:17:25 -0500

    Create reflective pyramid + outline

--- src/sketches/S011/sketch.js
+++ src/sketches/S011/sketch.js
@@ -17,8 +17,7 @@ class Sketch extends SketchManagerThree {
     this.composer = {};
 
     this.pyramid = {};
-    this.material = {};
-    this.geometry = {};
+    this.outlilneMaterial = {};
   }
   unmount() {
 
@@ -26,13 +25,53 @@ class Sketch extends SketchManagerThree {
   init() {
     // this.disableOrbitControls();
     this.setClearColor(0x111111)
-    this.setCameraPos(0, 0, -100);
+    this.setCameraPos(0, 0, -50);
     this.lookAt(0, 0, 0);
+
+    this.createPyramid();
+    this.createTest();
   }
   createPyramid() {
+    this.cubeCamera = new THREE.CubeCamera(1, 1000, 90);
+    this.scene.add(this.cubeCamera);
 
+    const geometry = new THREE.ConeGeometry(20, 30, 4);
+
+    const mirrorMaterial = new THREE.MeshBasicMaterial({
+      color: 0xddddff,
+      envMap: this.cubeCamera.renderTarget,
+    });
+    const mirrorPyramid = new THREE.Mesh(geometry, mirrorMaterial);    
+
+    const outlineMaterial = new THREE.RawShaderMaterial({
+      wireframe: true,
+      vertexShader: vert,
+      fragmentShader: frag,
+    });
+    const outlinePyramid = new THREE.Mesh(geometry, outlineMaterial);
+
+    this.pyramid = new THREE.Group();
+    this.pyramid.add(mirrorPyramid);
+    this.pyramid.add(outlinePyramid);
+    this.scene.add(this.pyramid)
+
+    this.pyramid.rotation.x += utils.toRadians(180);
   }
+  /**
+   * to test reflection
+   */
+  createTest() {
+    const test = new THREE.Mesh(
+      new THREE.BoxGeometry(10, 2,2),
+      new THREE.MeshBasicMaterial({ color: 0xddddff })
+      );
+      test.position.set(1, -10, 20);
+      this.scene.add(test);
+  }
+
   draw() {
+    this.cubeCamera.position.copy(this.pyramid.position);
+    this.cubeCamera.update(this.renderer, this.scene);
     this.renderer.render(this.scene, this.camera);
 
     requestAnimationFrame(() => this.draw());
