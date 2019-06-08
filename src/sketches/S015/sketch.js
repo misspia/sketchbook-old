8cd3ba8a1ce697538ae707e776a69d5065df54a8
misspia <pia_leung@hotmail.com>
2019-06-08 17:29:13 -0400

    init audio

--- src/sketches/S015/sketch.js
+++ src/sketches/S015/sketch.js
@@ -1,13 +1,19 @@
 import * as THREE from 'three';
 import SketchManagerThree from '../sketchManagerThree';
 import utils from '../utils';
+import { Audio } from '../../themes/themes';
 
-
+/**
+ * https://twitter.com/motions_work/status/927346292283490305
+ */
 class Sketch extends SketchManagerThree {
-  constructor(canvas) {
-    super(canvas);
-
-
+  constructor(canvas, audioElement) {
+    super(canvas, audioElement);
+    this.audioSrc = Audio.tester;
+    this.fftSize = 512;
+    this.numFrequencyNodes = 125;
+    this.spheres = [];
+    
   }
   unmount() {
 
@@ -17,9 +23,30 @@ class Sketch extends SketchManagerThree {
     this.lookAt(0, 0, 0);
     this.setClearColor(0xddddff);
 
+    const audioConfig = {
+      fftSize: this.fftSize,
+      dataLength: this.numFrequencyNodes,
+    };
+    this.initAudio(audioConfig);
+    
+    this.initNodes();
+
+  }
+  initNodes() {
+    this.spheres = this.audio.frequencyData.map((node, index) => {
+      const geometry = new THREE.SphereGeometry(5, 32, 32);
+      const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+      });
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.z += index;
+      this.scene.add(mesh);
+      return mesh;
+    });
   }
   draw() {
     this.renderer.render(this.scene, this.camera);
+    this.audio.getByteFrequencyData();
    
 
     requestAnimationFrame(() => this.draw());
