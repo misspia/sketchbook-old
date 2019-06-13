22b7c6252d684c99118152da31fb95ff681e2032
misspia <pia_leung@hotmail.com>
2019-06-13 15:27:12 -0400

    remove audio viz, initialize leaf nodes

--- src/sketches/S015/leaf.js
+++ src/sketches/S015/leaf.js
@@ -4,7 +4,11 @@ import vert from './leaf.vert';
 
 export default class Leaf {
   constructor() {
-
+    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    this.material = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+    });
+    this.mesh = new THREE.Mesh(geometry, this.material);
   }
   update() {
 
--- src/sketches/S015/sketch.js
+++ src/sketches/S015/sketch.js
@@ -1,19 +1,21 @@
 import * as THREE from 'three';
 import SketchManagerThree from '../sketchManagerThree';
 import utils from '../utils';
-import { Audio } from '../../themes/themes';
 import Sphere from './sphere';
+import Leaf from './leaf';
 
 /**
  * https://twitter.com/motions_work/status/927346292283490305
+ * https://twitter.com/felixfaire/status/979094649653612544 r
  */
 class Sketch extends SketchManagerThree {
-  constructor(canvas, audioElement) {
-    super(canvas, audioElement);
-    this.audioSrc = Audio.tester;
-    this.fftSize = 512;
-    this.numFrequencyNodes = 125;
+  constructor(canvas) {
+    super(canvas);
     this.spheres = [];
+
+    this.numLeaves = 100;
+    this.leaves = [];
+
     
   }
   unmount() {
@@ -21,50 +23,21 @@ class Sketch extends SketchManagerThree {
   }
   init() {
     this.setCameraPos(-32, 74, -77);
+    this.lookAt(0, 0, 0);
     this.setClearColor(0xddddff);
 
-    const audioConfig = {
-      fftSize: this.fftSize,
-      dataLength: this.numFrequencyNodes,
-    };
-    this.initAudio(audioConfig);
-    
-    this.createNodes();
-    const nodesGroup = new THREE.Group();
-    this.spheres.forEach(sphere => nodesGroup.add(sphere.mesh.clone()));
-    const { x, y, z } = new THREE.Box3().setFromObject(nodesGroup).getCenter(nodesGroup.position).multiplyScalar(0.5)
+    this.createLeaves();
 
   }
-  createNodes() {
-    let x = 0;
-    let y = 0;
-    let z = 0;
-    const gap = 15;
-    this.audio.frequencyData.map((node, index) => {
-      const sphere = new Sphere();
-      
-      if(index % 5 === 0) {
-        x = 0;
-        y += gap;
-      }
-      if(index % 25 === 0) {
-        y = 0;
-        z += gap;
-      }
-      x += gap;
-      sphere.mesh.position.x += x;
-      sphere.mesh.position.y += y;
-      sphere.mesh.position.z += z;
-      this.scene.add(sphere.mesh);
-      this.spheres.push(sphere);
-    }); // why doesnt map work (returns array of 0's)
+  createLeaves() {
+    for(let i = 0; i < this.numLeaves; i++) {
+      const leaf = new Leaf();
+      this.leaves.push(leaf);
+      this.scene.add(leaf.mesh);
+    }
   }
   draw() {
     this.renderer.render(this.scene, this.camera);
-    this.audio.getByteFrequencyData();
-    this.audio.frequencyData.forEach((frequency, index) => {
-      this.spheres[index].update(frequency);
-    })
    
 
     requestAnimationFrame(() => this.draw());
