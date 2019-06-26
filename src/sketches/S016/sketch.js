1e753642b3ae46656545f84ba842e520acab3380
misspia <pia_leung@hotmail.com>
2019-06-26 14:33:48 -0400

    create triangle class

--- src/sketches/S016/sketch.js
+++ src/sketches/S016/sketch.js
@@ -1,8 +1,17 @@
 import * as THREE from 'three';
 import SketchManagerThree from '../sketchManagerThree';
-import { Audio } from '../../themes/themes.js';
+import { Audio } from '../../themes/themes';
+import Triangle from './triangle';
 import utils from '../utils';
 
+/**
+ * https://github.com/mattdesl/codevember/blob/gh-pages/src/21.js
+ * https://www.pinterest.ca/pin/567101778077936381/
+ * https://codepen.io/ykob/pen/qbwLaY?editors=0010
+ * 
+ * beat
+ * https://codepen.io/mnmxmx/pen/mmZbPK?editors=1010
+ */
 class Sketch extends SketchManagerThree {
   constructor(canvas, audioElement) {
     super(canvas, audioElement);
@@ -17,10 +26,13 @@ class Sketch extends SketchManagerThree {
 
     this.setCameraPos(-32, 74, -77);
     this.lookAt(0, 0, 0);
-    this.setClearColor(0xeefaee);
+    this.setClearColor(0xffffaa);
 
     const audioConfig = { fftSize: this.fftSize };
     this.initAudio(audioConfig);
+
+    const triangle = new Triangle();
+    this.scene.add(triangle.mesh);
 
   }
   
