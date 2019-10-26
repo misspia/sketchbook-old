c173f6470f78d2a64c33afc66628e1065d438cbc
misspia <pia_leung@hotmail.com>
2019-10-26 23:29:57 +0200

    set up audio

--- src/sketches/S015/sketch.js
+++ src/sketches/S015/sketch.js
@@ -2,22 +2,34 @@ import * as THREE from 'three';
 import SketchManagerThree from '../sketchManagerThree';
 import utils from '../utils';
 import Leaf from './leaf';
+import { Audio } from '../../themes/themes';
 
 /**
  * https://twitter.com/felixfaire/status/979094649653612544
  */
 class Sketch extends SketchManagerThree {
-  constructor(canvas) {
-    super(canvas);
+  constructor(canvas, audioElement) {
+    super(canvas, audioElement);
     this.clock = new THREE.Clock();
+    this.numFrequencyNodes = 100;
+    this.fftSize = 512;
+
     this.numLeaves = 100;
     this.leaves = [];
+    this.audioSrc = Audio.tester;
   }
   unmount() {
 
   }
   init() {
     this.createStats();
+
+    const audioConfig = {
+      fftSize: this.fftSize,
+      dataLength: this.numFrequencyNodes,
+    };
+    this.initAudio(audioConfig);
+    this.audio.volume(1);
 
     this.setCameraPos(-32, 74, -77);
     this.lookAt(0, 0, 0);
@@ -36,9 +48,15 @@ class Sketch extends SketchManagerThree {
   draw() {
     this.stats.begin();
 
+    const time = this.clock.getElapsedTime();
+
+    this.audio.getByteFrequencyData();
+    this.audio.frequencyData.forEach((frequency, index) => {
+      this.leaves[index].update(time, frequency);
+    });
+
     this.renderer.render(this.scene, this.camera);
 
-    const time = this.clock.getElapsedTime();
     this.leaves.forEach(leaf => leaf.update(time));
    
     this.stats.end();
